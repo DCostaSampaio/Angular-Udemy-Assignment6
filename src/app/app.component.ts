6faeb18977a7ed3f8077a5c76b7c070d1b9e7985
DCostaSampaio <diogocostasampas5@gmail.com>
2023-03-16 18:58:42 +0000

    Assignment 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomVal } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Assignment-6';
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, CustomVal.invalidName, CustomVal.asyncInvalidName]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('critical')
+    });
+  }
+
+  onSave() {
+    console.log(this.projectForm.value);
+  }
+
+
 }
